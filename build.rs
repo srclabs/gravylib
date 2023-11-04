1ae45cbf61c1a718aa506047ac5ba795859beb6f
Dr. Rubisco <76263371+thedocruby@users.noreply.github.com>
2023-11-04 12:24:02 -0400

    Revert "Finish move"
    This reverts commit 9940b3b4a765602250b7ca699f0ab5884178ee09.

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use spirv_builder::{SpirvBuilder, MetadataPrintout};
+
 use std::env;
 use std::error::Error;
 use std::path::PathBuf;
@@ -21,13 +23,13 @@ fn main() -> Result<(), Box<dyn Error>> {
         && dir.ends_with(profile)
         && dir.pop();
     assert!(ok);
-    // NOTE (eddyb) this needs to be distinct from the `--target-dir` value that
+    // NOTE(eddyb) this needs to be distinct from the `--target-dir` value that
     // `spirv-builder` generates in a similar way from `$OUT_DIR` and `$PROFILE`,
     // otherwise repeated `cargo build`s will cause build script reruns and the
     // rebuilding of `rustc_codegen_spirv` (likely due to common proc macro deps).
     let dir = dir.join("gravylib-builder");
     let status = std::process::Command::new("cargo")
-        .args([  // TODO: Find a better way to do this
+        .args([
             "run",
             "--release",
             "-p",
@@ -46,6 +48,19 @@ fn main() -> Result<(), Box<dyn Error>> {
             std::process::exit(1);
         }
     }
+
+    // Internal shader-like dependencies, built alongside `gravylib`
+    SpirvBuilder::new("gravylib-helpers", "spirv-unknown-vulkan1.1")
+        .print_metadata(MetadataPrintout::Full)
+        .build()?;
+
+    // External shaders, should be built alongside the dependent crate
+    // Built alongside `gravylib` because it's needed for the tests/examples
+    // TODO: Look into a way to build this only when needed (i.e when running tests/examples)
+    // TODO: This is kinda boilerplate... Can we abstract this for devs with `gravylib_macros`?
+    SpirvBuilder::new("examples/shaders", "spirv-unknown-vulkan1.1")
+        .print_metadata(MetadataPrintout::Full)
+        .build()?;
 
     Ok(())
 }
--- gravylib-builder/src/bin/main.rs
+++ gravylib-builder/src/bin/main.rs
@@ -2,13 +2,7 @@ use gravylib_builder::*;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Internal shader-like dependencies, built alongside `gravylib`
     build_shader("../gravylib-helpers")?;
-
-    // External shaders, should be built alongside the dependent crate
-    // Built alongside `gravylib` because it's needed for the tests/examples
-    // TODO: Look into a way to build this only when needed (i.e when running tests/examples)
     build_shader("../examples/shaders")?;
-
     Ok(())
 }
